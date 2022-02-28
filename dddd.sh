08a9c3d091cccbc67ea0f885765f76808ae9d5dd
Quintin Shaw <github@xyt.email>
2022-02-28 17:47:34 +0000

    Add copyright
    Signed-off-by: Quintin Shaw <github@xyt.email>

--- dddd.sh
+++ dddd.sh
@@ -2,3 +2,13 @@
 
 #check root permission
 [ $EUID -ne 0 ] && clear && echo "Error: This script must be run as root!" 1>&2 && exit 1
+
+function CopyRight() {
+    clear
+    echo "------------------- A dddd.sh Script By QuintinShaw ------------------"
+    echo " Version            : v2022-02-28"
+    echo " Usage              : wget -qO- dddd.sh | bash"
+    echo "----------------------------------------------------------------------"
+}
+
+CopyRight
