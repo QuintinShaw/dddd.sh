03385c3976f6341dbcb7e3d621005a97195bb3be
Quintin Shaw <github@xyt.email>
2022-02-28 18:33:56 +0000

    Add color for text

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dddd.sh
+
+## Reference
+* [AutoReinstall.sh](https://github.com/hiCasper/Shell/blob/master/AutoReinstall.sh)
+* [bench.sh](https://github.com/teddysun/across/blob/master/bench.sh)
+* [yabs.sh](https://github.com/masonr/yet-another-bench-script/blob/master/yabs.sh)
--- dddd.sh
+++ dddd.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
 
 #check root permission
-[ $EUID -ne 0 ] && clear && echo "Error: This script must be run as root!" 1>&2 && exit 1
+#[ $EUID -ne 0 ] && clear && echo "Error: This script must be run as root!" 1>&2 && exit 1
 
-function CopyRight() {
+red() {
+    printf '\033[0;31m%b\033[0m' "$1"
+}
+
+green() {
+    printf '\033[0;32m%b\033[0m' "$1"
+}
+
+yellow() {
+    printf '\033[0;33m%b\033[0m' "$1"
+}
+
+blue() {
+    printf '\033[0;34m%b\033[0m' "$1"
+}
+
+copyright() {
     clear
     echo "------------------- A dddd.sh Script By QuintinShaw ------------------"
-    echo " Version            : v2022-02-28"
-    echo " Usage              : wget -qO- dddd.sh | bash"
+    echo " Version            : $(green v2022-02-28)"
+    echo " Usage              : $(yellow "wget -qO- dddd.sh | bash")"
     echo "----------------------------------------------------------------------"
 }
 
-CopyRight
+copyright
